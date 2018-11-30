f86b6736b9f36c3efbb52990ad48c1b5810a0097
ivan4000 <sxln1@mail.ru>
2018-11-30 21:21:07 +0300

    JL297 business de;egate

---
+++ src/com/Main.java
@@ -0,0 +1,57 @@
+package com;
+
+public class Main {
+    public static void main(String[] args) {
+        BusinessDelegate businessDelegate=new BusinessDelegate();
+        businessDelegate.doTask("ejb");
+    }
+}
+
+interface BusinessService {
+    void doJob();
+}
+
+class EJBService implements BusinessService {
+    @Override
+    public void doJob() {
+        System.out.println("do ejb job");
+    }
+}
+
+class JMSService implements BusinessService {
+    @Override
+    public void doJob() {
+        System.out.println("do jms job");
+    }
+}
+
+class LookupService {
+    BusinessService getService(String serviceType) {
+        if (serviceType.equals("ejb")) {
+            return new EJBService();
+        } else {
+            return new JMSService();
+        }
+    }
+}
+class BusinessDelegate{
+    LookupService lookupService=new LookupService();
+    BusinessService businessService;
+    void doTask(String serviceType){
+        businessService=lookupService.getService(serviceType);
+        businessService.doJob();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
